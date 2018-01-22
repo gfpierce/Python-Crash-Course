67baf12e88fe84400d527e7266f1076ff1382cd7
gfpierce <31076033+gfpierce@users.noreply.github.com>
2018-01-21 22:07:14 -0500

    Chapter 4
    - completed code for Chapter 4: Working With Lists

---
+++ lists_work.py
@@ -0,0 +1,10 @@
+foods = ('fried chicken', 'pizza', 'corn', 'fries', 'steak')
+
+for food in foods:
+    print(food.title())
+
+foods = ('fried chicken', 'pizza', 'broccoli', 'fries', 'tenderloin')
+
+print('\nRevised menu: ')
+for food in foods:
+    print(food)
