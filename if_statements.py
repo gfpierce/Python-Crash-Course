6ef3156a8234f1742dd61f3fa0047a177e328e7f
gfpierce <31076033+gfpierce@users.noreply.github.com>
2018-01-23 20:08:17 -0500

    Chapter 5
    - code for chapter 5 'if statements'

---
+++ if_statements.py
@@ -0,0 +1,70 @@
+# cars.py
+# cars = ['audi', 'bmw', 'subaru', 'toyota']
+#
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+
+# toppings.py
+# requested_topping = 'mushroom'
+#
+# if requested_topping != 'anchovies':
+#     print("Hold the anchovies!")
+
+# try it yourself 1
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+#
+# print("\nIs car == 'audi'? I predict False.")
+# print(car == 'audi')
+
+# pizza_toppings = ['sausage', 'pepperoni', 'margherita']
+# print('sausage' in pizza_toppings)
+# print('margherita' in pizza_toppings)
+# print('pineapple' in pizza_toppings)
+# print('pepperoni' in pizza_toppings)
+# print('onion' in pizza_toppings)
+#
+# check_num = 5
+# for number in range(1, 6):
+#     print(number == check_num)
+
+# check_string = 'Hello'
+#
+# print(check_string.lower() == 'Hello')
+# print(check_string.lower() == 'hello')
+
+# try it yourself 2
+# alien_color = 'green'
+# if alien_color == 'green':
+#     print("You have earned 5 points.")
+# elif alien_color == 'yellow':
+#     print("You have earned 5 points.")
+# elif alien_color == 'red':
+#     print("You have earned 5 points.")
+
+# alien_color = 'green'
+# if alien_color == 'green':
+#     print("You have earned 5 points for shooting a green alien.")
+# elif alien_color == 'yellow':
+#     print("You have earned 10 points for shooting a yellow alien.")
+# elif alien_color == 'red':
+#     print("You have earned 15 points for shooting a red alien.")
+
+age = 14
+if age < 2:
+    print("This person is a baby.")
+elif age < 4:
+    print("This person is a toddler.")
+elif age < 13:
+    print("This person is a kid.")
+elif age < 20:
+    print("This person is a teenager.")
+elif age < 65:
+    print("This person is an adult.")
+elif age >=65:
+    print("This person is a elder.")
+
